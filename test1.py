68f459ecd20a799412c3ad5d3f7572c8ac9c62d9
Tonkl7 <tonkakaxd@gmail.com>
2026-04-07 19:05:21 +0700

    add test1.py

---
+++ test1.py
@@ -0,0 +1,88 @@
+#------ test 1 --------------------------
+#num = int(input("input your Num : "))
+#flt = float(input("input your float : "))
+
+#print(f'Sum : {num + flt}')
+#print(f'Minus : {num - flt}')
+#print(f'Power : {num * flt}')
+#print(f'Divide : {num / flt}')
+
+#------- test 2 -------------------------
+#x = "15"
+#y = "3"
+
+#print(f'sum {int(x) + int(y)}')
+#print(f'multi {int(x) * int(y)}')
+#print(f'power {int(x) ** int(y)}')
+
+#------ test 3 -------------------------text = "Python Programing"
+
+#print(text[2:0])
+#text = "Python Programming"
+
+#print(text[0])
+#print(text[-1])
+#print(text[0:7])
+#print(text[-6:])
+
+
+
+
+#----- test 4 --------------------------
+#text = str(input())
+
+#print(text.upper())
+#print(text.lower())
+#print(text.capitalize())
+#print(len(text))
+#print(text.replace('o' , 'a'))
+
+#------ test 5 -------------------------
+#email = "student@gmail.com"
+#email = email.split("@")
+
+#print("username = " + email[0])
+#print("domain = " + email[1])
+
+#------ test 6 -------------------------
+#sentence = "I love Python very much"
+#sentence = sentence.split()
+#count = len(sentence)
+
+#print(count)
+#print('-'.join(sentence))
+#----- test 7 --------------------------
+#text = input()
+
+#print(text[1])
+#print(text[2:5])
+#print(text[::2])
+#print(text[::-1])
+#print(text[-3:])
+
+#----- test 8 --------------------------
+#code = "A123B456C789"
+
+#print(code[0])
+#print(code[1:4])
+#print(code[4])
+#print(code[5:8])
+#print(code[8])
+#print(code[9:])
+#----- test 9 --------------------------
+#name = "Tonkla"
+#age = 16
+#score = 99.99
+
+#print(f'My name is {name}, I am {age} years old, My score is {score}') 
+#----- test 10 -------------------------
+#text = " python is FUN "
+#text = text.strip()
+#text = text.replace("FUN" , "fun")
+#text = text.replace("python" , "PYTHON")
+
+#print(text)
+#print(f'has python : {text.find("python")}')
+#print(f'start with python : {text.startswith("python")}')
+#print(f'end with FUN : {text.endswith("FUN")}')
+
